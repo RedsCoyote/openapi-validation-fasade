28e563fdf513c8c9a3b81d8fa0c813eff8fed41a
Дмитрий Макаров <d.makarov@webbankir.com>
2021-05-01 23:25:40 +0300

    Перехват первых ошибок

--- src/OpenApiValidatorTrait.php
+++ src/OpenApiValidatorTrait.php
@@ -4,24 +4,69 @@ declare(strict_types=1);
 
 namespace App;
 
+use Mmal\OpenapiValidator\Exception\InvalidSchemaException;
+use Mmal\OpenapiValidator\Validator;
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
 
 trait OpenApiValidatorTrait
 {
     /**
      * Проверяет соответствие ответа схеме.
      *
-     * @param string            $schemaPath Путь к файлу документации.
-     * @param string            $path       Путь метода API.
-     * @param string            $method     Метод запроса.
-     * @param ResponseInterface $response   Проверяемый ответ.
+     * @param string            $schemaPath    Путь к файлу документации.
+     * @param string            $requestPath   Путь запроса.
+     * @param string            $requestMethod Метод запроса.
+     * @param ResponseInterface $response      Проверяемый ответ.
+     * @param string            $contentType   Тип содержимого ответа.
+     *
+     * @throws AssertionFailedError
      */
     public function validateResponseAgainstScheme(
         string $schemaPath,
-        string $path,
-        string $method,
-        ResponseInterface $response
+        string $requestPath,
+        string $requestMethod,
+        ResponseInterface $response,
+        string $contentType = 'application/json'
     ): void {
+        try {
+            $parsedSchema = Yaml::parseFile($schemaPath);
+        } catch (ParseException $e) {
+            TestCase::fail(
+                \sprintf('Неправильный формат спецификации "%s": %s', $schemaPath, $e->getMessage())
+            );
+        }
 
+        if (!\is_array($parsedSchema)) {
+            TestCase::fail(
+                \sprintf('Файл спецификации "%s" пустой.', $schemaPath)
+            );
+        }
+
+        try {
+            $validator = new Validator($parsedSchema);
+            $result = $validator->validateBasedOnRequest(
+                $requestPath,
+                $requestMethod,
+                $response->getStatusCode(),
+                \json_decode((string) $response->getBody(), true, 512, JSON_THROW_ON_ERROR),
+                $contentType
+            );
+
+            if ($result->hasErrors()) {
+                TestCase::fail(
+                    ''
+                );
+            }
+        } catch (InvalidSchemaException $e) {
+            TestCase::fail(
+                ''
+            );
+        } catch (\JsonException $e) {
+            TestCase::fail();
+        }
     }
 }
--- tests/Unit/OpenApiValidatorTraitTest.php
+++ tests/Unit/OpenApiValidatorTraitTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
+use App\OpenApiValidatorTrait;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use PHPUnit\Framework\AssertionFailedError;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Тест примеси проверки соответствия ответов схеме.
@@ -13,4 +18,104 @@ use PHPUnit\Framework\TestCase;
  */
 class OpenApiValidatorTraitTest extends TestCase
 {
+    /**
+     * Путь к схеме openApi v3.
+     */
+    private const SCHEMA_PATH = __DIR__ . '/schemes/schema_1.yaml';
+
+    /**
+     * Путь к пустому файлу схемы.
+     */
+    private const EMPTY_FILE_PATH = __DIR__ . '/schemes/empty_file.yaml';
+
+    /**
+     * Путь к пустому файлу схемы.
+     */
+    private const INVALID_FILE_PATH = __DIR__ . '/schemes/invalid_format.yaml';
+
+    /**
+     * Проверяемая прослойка.
+     *
+     * @var MockObject&OpenApiValidatorTrait
+     */
+    private $validatorTrait;
+
+    /**
+     * Проверяет обработку ответа, соответствующего схеме.
+     *
+     * @throws \Throwable
+     */
+    public function testValidate(): void
+    {
+        $this->validatorTrait->validateResponseAgainstScheme(
+            self::SCHEMA_PATH,
+            '/foo/42/bar?baz=24',
+            'PUT',
+            $this->createResponse()
+        );
+    }
+
+    /**
+     * Проверяет обработку ошибки, если результат парсинга схемы не массив, например, файл пустой.
+     *
+     * @throws \Throwable
+     */
+    public function testEmptySchemaProcessing(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches(
+            '~Файл спецификации ".*" пустой\.~'
+        );
+
+        $this->validatorTrait->validateResponseAgainstScheme(
+            self::EMPTY_FILE_PATH,
+            '/foo/42/bar?baz=24',
+            'PUT',
+            $this->createResponse()
+        );
+    }
+
+    /**
+     * Проверяет обработку ошибки, если парсинга схемы не удался.
+     *
+     * @throws \Throwable
+     */
+    public function testSchemaParsingError(): void
+    {
+        $this->expectException(AssertionFailedError::class);
+        $this->expectExceptionMessageMatches(
+            '~Неправильный формат спецификации ".*": Duplicate key "bar" detected at line 3 \(near "bar: 43"\)\.~'
+        );
+
+        $this->validatorTrait->validateResponseAgainstScheme(
+            self::INVALID_FILE_PATH,
+            '/foo/42/bar?baz=24',
+            'PUT',
+            $this->createResponse()
+        );
+    }
+
+    /**
+     * Готовит окружение теста.
+     *
+     * @throws \Throwable
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->validatorTrait = $this->getMockForTrait(OpenApiValidatorTrait::class);
+    }
+
+    /**
+     * Возвращает тестовый ответ метода.
+     *
+     * @return ResponseInterface
+     *
+     * @throws \Throwable
+     */
+    private function createResponse(): ResponseInterface
+    {
+        return (new Psr17Factory())->createResponse();
+    }
 }
